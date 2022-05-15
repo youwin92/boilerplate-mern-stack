f91a0f281c51a3bbe87cc4112fd159781b2f0f0e
youwin92 <lucyte92@gmail.com>
2022-05-15 21:32:24 +0900

    product 기능 완료

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,6 +1,7 @@
 import React,{ useState} from "react";
 import { Typography, Button, Form, Input} from "antd";
 import FileUpload from "../../utils/FileUpload";
+import axios from 'axios';
 
 
 // const {Title} = Typography;
@@ -15,7 +16,7 @@ const Continents =[
   {key:6, value:"오스트레일리아"}
 ];
 
-function UploadProductPage(){
+function UploadProductPage(props){
 
   const [Title, setTitle] = useState("");
   const [Description, setDescription] = useState("");
@@ -40,13 +41,43 @@ function UploadProductPage(){
   const updateImages = (newImages) =>{
     setImages(newImages);
   }
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if(!Title || !Description || !Price || !Continent || !Images){
+      return alert("모든 값을 넣어 주셔야 합니다");
+    }
+
+    //서버에 채운 값들을 request로 보낸다
+    const body = {
+      //로그인 된 사람의 ID
+      writer: props.user.userData._id,
+      title: Title,
+      description: Description,
+      price: Price,
+      images: Images,
+      continents : Continent
+    };
+
+    axios.post("/api/product/", body)
+      .then(response =>{
+        if( response.data.success){
+          alert("상품 업로드 성공");
+          //업로드 이후 렌딩페이지로 이동
+          props.history.push("/");
+        }else{
+          alert("상품 업로드 실패");
+        }
+      })
+  }
+
   return(
       <div style={{maxWidth:'700px', margin: '2rem auto' }}>
         <div style={{ textAlign: 'center', marginBottom: '2rem'}}>
           <h2 level={2}>여행 상품 업로드</h2>
         </div>
 
-        <Form>
+        <Form onSubmitCapture={submitHandler}>
          <FileUpload refreshFunction={updateImages}/>
 
 
@@ -72,7 +103,7 @@ function UploadProductPage(){
           </select>
           <br /> 
           <br /> 
-          <Button>확인</Button>
+          <Button htmlType='submit'>확인</Button>
         </Form>
       </div>
   )
--- server/routes/product.js
+++ server/routes/product.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require("multer");
+const { Product } = require("../models/Product");
 
 //=================================
 //             Product
@@ -28,5 +29,18 @@ router.post("/image", (req,res)=>{
     return res.json({ success: true, filePath: res.req.file.path, fileName: res.req.file.filename })
   });
 });
+
+router.post("/", (req,res)=>{
+  //받아온 정보들을 db에 넣어준다.
+
+  const product = new Product(req.body);
+  product.save((err) => {
+    if( err) return res.status(400).json({success : false, err});
+    return res.status(200).json({success: true});
+
+  });
+
+});
+
 
 module.exports = router;

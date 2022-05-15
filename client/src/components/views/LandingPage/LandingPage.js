43696e09f2cc890a7bf40098750990f5766f8f99
youwin92 <lucyte92@gmail.com>
2022-05-15 22:23:02 +0900

    이미지 렌더

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,19 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import { FaCode } from "react-icons/fa";
 import axios from "axios";
-
+import {Icon, Col, Card, Row} from 'antd';
+import Meta from 'antd/lib/card/Meta';
 
 function LandingPage() {
 
+  const [Products, setProducts] = useState([]);
 
   useEffect( () => {
     axios.post("/api/product/products")
       .then( response => {
         if(response.data.success){
           console.log( response.data);
+          setProducts(response.data.productInfo);
         }else{
           alert("상품들을 가져오는데 실패");
        }
@@ -19,10 +22,40 @@ function LandingPage() {
   }, [])
 
 
+  const renderCard = Products.map((product, index) => {
+
+    console.log( 'product',product);
+
+    return <Col lg={6} md={8} xs={24} key={index}>
+      <Card
+        cover={<img style={{ width:'100%',maxHeight:'150px'}} src={`http://localhost:5000/${product.images[0]}`} />}
+      >
+      <Meta
+        title={product.title}
+        description={`${product.price}`}
+      />
+      </Card>
+    </Col>;
+  })
 
     return (
-        <div>
-          LandingPage
+        <div style={{width:'75%', margin: '3rem auto'}}>
+          <div style={{ textAlign: "center"}}>
+            <h2>고고고~!<Icon type="rocket"/></h2>
+          </div>
+          {/*Filter*/}
+          {/*Search*/}
+
+          {/*Cards*/}
+          <Row gutter={16}>
+            {renderCard}
+          </Row>
+
+
+          <div style={{ display: "flex",  justifyContent:'center'}}>
+            <button>더보기</button>
+
+          </div>
         </div>
     )
 }
